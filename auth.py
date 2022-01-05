edb6e06a3612455377e651547a3f38aed8bf7440
Tyler-Inari <s1922054@stu.musashino-u.ac.jp>
2022-01-05 22:46:51 +0900

    get error because of accesstoken

--- auth.py
+++ auth.py
@@ -2,19 +2,20 @@ import base64
 
 import environ
 import requests
+from requests.models import Response
 
 
 class Auth:
     def __init__(self):
         self.env = environ.Env()
         self.env.read_env(".env")
-        self.username, self.client_id, self.client_secret, self.scope = (
-            self.env("USERNAME"),
+        self.username = self.env("USERNAME")
+        self.client_id, self.client_secret = (
             self.env("CLIENT_ID"),
             self.env("CLIENT_SECRET"),
-            self.env("SCOPE"),
         )
         self.redirect_uri = self.env("REDIRECT_URI")
+        self.scope = self.env("GRPT_SCOPE")
 
     def accessToken(self):
         url = "https://accounts.spotify.com/api/token"
@@ -23,8 +24,18 @@ class Auth:
         message = f"{self.client_id}:{self.client_secret}"
         base64Bytes = base64.b64encode(message.encode())
         headers["Authorization"] = f"Basic {base64Bytes.decode()}"
+        # data["username"] = self.username
+        # data["client_id"] = self.client_id
+        # data["client_secret"] = self.client_secret
         data["grant_type"] = "client_credentials"
+        # data["response_type"] = "code"
+        data["response_type"] = "token"
+        data["redirect_uri"] = self.redirect_uri
+        data["scope"] = self.scope
         auth_response = requests.post(url, headers=headers, data=data)
+        print("----------------------------------------")
+        print(auth_response.json())
+        print("----------------------------------------")
         access_token = auth_response.json()["access_token"]
         return access_token
 
--- main.py
+++ main.py
@@ -1,58 +1,29 @@
-import os
-# import copy
-import json
-import datetime
-# import pandas as pd
-import auth_spotify
+import environ
+import requests
 
-def main():
-    # record time for filename
-    dt_now = datetime.datetime.now()
-    jst_list = [dt_now.year, dt_now.month, dt_now.day, dt_now.hour]
-    time_list = ['0'+str(time)  if len(str(time)) == 1 else str(time) for time in jst_list]
-    idir_path = 'data'
-    if not os.path.isdir(idir_path):
-        os.makedirs(idir_path)
-    json_path = f'{idir_path}/{time_list[0]}{time_list[1]}{time_list[2]}{time_list[3]}_recently.json'
-    csv_path = f'{idir_path}/{time_list[0]}{time_list[1]}{time_list[2]}{time_list[3]}_recently.csv'
+from auth import Auth
 
-    # setup
-    auth = auth4spotify.Auth()
-    spotify = auth.authWithScope()
 
-    recently_played = spotify.current_user_recently_played(limit=50)
-    with open(json_path, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(recently_played))
-    print('path is', json_path)
+class GetRecentlyPlayedTracks:
+    def __init__(self):
+        self.auth = Auth()
+        self.env = environ.Env()
+        self.env.read_env(".env")
+        self.scope = (self.env("GRPT_SCOPE"),)
+        self.grpt_endpoint = self.env("GRPT_ENDPOINT")
 
-    # df = pd.json_normalize(recently_played['items'])
+    def get_recently_played_tracks(self):
+        access_token = self.auth.accessToken()
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        res = requests.get(self.grpt_endpoint, headers=headers).json()
+        return res
 
-    # # backup
-    # df['track.artists.backup'] = copy.deepcopy(df['track.artists'])
-    # # convert json to artist name
-    # artists = []
-    # for values in df['track.artists']:
-    #     artists_ = ''
-    #     if len(values) >= 2:
-    #         for val in values:
-    #             artists_ += val.get('name') + ', '
-    #         artists_ = artists_[:-2]
-    #         artists.append(artists_)
-    #     else:
-    #         for val in values:
-    #             artists.append(val.get('name'))
 
-    # # update artists
-    # df['track.artists'] = artists
-
-    # # convert to jst
-    # df = df.sort_values('played_at').reset_index(drop=True)
-    # df['played_at'] = pd.to_datetime(df['played_at'])
-    # df['played_at'] = [played_at.tz_convert('Asia/Tokyo') for played_at in df['played_at']]
-
-    # # save data as csv
-    # df.to_csv(csv_path)
-    # print('path is', csv_path)
-
-if __name__=='__main__':
-    main()
+if __name__ == "__main__":
+    grpt = GetRecentlyPlayedTracks()
+    res = grpt.get_recently_played_tracks()
+    print(res)
