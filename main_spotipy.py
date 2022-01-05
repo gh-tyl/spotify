90b56b52e8a7d74fe2b5bc360d6fb1f8209b9a80
Tyler-Inari <s1922054@stu.musashino-u.ac.jp>
2022-01-05 23:50:47 +0900

    add zip for lambda

--- main_spotipy.py
+++ main_spotipy.py
@@ -5,7 +5,7 @@ import os
 import auth_spotipy
 
 
-def main():
+def main(event=False, context=False):
     # record time for filename
     dt_now = datetime.datetime.now()
     jst_list = [dt_now.year, dt_now.month, dt_now.day, dt_now.hour]
@@ -14,7 +14,12 @@ def main():
     ]
     idir_path = "data"
     if not os.path.isdir(idir_path):
-        os.makedirs(idir_path)
+        try:
+            os.makedirs(idir_path)
+        except Exception as e:
+            idir_path = "/tmp/data"
+            os.makedirs(idir_path)
+            print(f"Error is {e}")
     json_path = f"{idir_path}/{time_list[0]}{time_list[1]}{time_list[2]}{time_list[3]}_recently.json"
 
     # setup
