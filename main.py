7d46069ef7ef95bc0a6c7322c140662c346e0a8b
Tyler-Inari <s1922054@stu.musashino-u.ac.jp>
2022-01-28 14:26:15 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import datetime
+import json
+import os
+
 import environ
 import requests
 
@@ -24,8 +28,29 @@ class GetRecentlyPlayedTracks:
         res = requests.get(self.grpt_endpoint, headers=headers).json()
         return res
 
+    def save_json(self, res):
+        # record time for filename
+        dt_now = datetime.datetime.now()
+        jst_list = [dt_now.year, dt_now.month, dt_now.day, dt_now.hour]
+        time_list = [
+            "0" + str(time) if len(str(time)) == 1 else str(time) for time in jst_list
+        ]
+        idir_path = "data"
+        if not os.path.isdir(idir_path):
+            try:
+                os.makedirs(idir_path)
+            except Exception as e:
+                idir_path = "/tmp/data"
+                os.makedirs(idir_path)
+                print(f"Error is {e}")
+        json_path = f"{idir_path}/{time_list[0]}{time_list[1]}{time_list[2]}{time_list[3]}_recently.json"
+        with open(json_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(res))
+        print("path is", json_path)
+
 
 if __name__ == "__main__":
     grpt = GetRecentlyPlayedTracks()
     res = grpt.get_recently_played_tracks()
-    print(res)
+    grpt.save_json(res)
+    # print(res)
